35cf4c2e83070dd5099c296ec0dbdaac2d6ca305
Sarthak Soni <sarthak21soni@gmail.com>
2026-01-30 16:33:58 +0530

    created different S3 buckets

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
 resource "aws_s3_bucket" "my-bucket" { 
 
-bucket = "soniji-terr-bucket" 
+bucket = "soniji-terr-bucket1" 
 
-} 
+}  
+
+resource "aws_s3_bucket" "my-bucket-2" { 
+
+bucket = "soniji-terr-bucket-2" 
+
+}  
+
+resource "aws_s3_bucket" "my-bucket-3" { 
+
+bucket = "soniji-terr-bucket-3" 
+
+}  
